4401c69da7934ee85a53e62d896b02ddaf7d3fce
superfeliX0 <superfelix0@users.noreply.github.com>
2026-07-23 03:53:48 +0900

    Load retention explicitly on home pages

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -6,15 +6,9 @@ class HeadInjector {
   }
 }
 
-class BodyInjector {
-  element(element) {
-    element.append('<script src="/retention.js?v=20260723"></script>', { html: true });
-  }
-}
-
 export const onRequest = async (context) => {
   const response = await context.next();
   const contentType = response.headers.get("content-type") || "";
   if (!contentType.includes("text/html")) return response;
-  return new HTMLRewriter().on("head", new HeadInjector()).on("body", new BodyInjector()).transform(response);
+  return new HTMLRewriter().on("head", new HeadInjector()).transform(response);
 };
